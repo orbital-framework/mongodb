20a7ad78080b3096c38c2bb4811a651347dcfa8c
Mateus Souza <mateussouzaweb@gmail.com>
2018-02-23 17:40:25 -0300

    Tabs to spaces conversion

--- app/Config/Mongodb.php
+++ app/Config/Mongodb.php
@@ -3,25 +3,25 @@
 // Development
 if( ENVIRONMENT == 'development' ):
 
-	App::set('mongodb', array(
-		'connection' => 'mongodb://mongodb:27017',
-		'database' => 'orbital'
-	));
+    App::set('mongodb', array(
+        'connection' => 'mongodb://mongodb:27017',
+        'database' => 'orbital'
+    ));
 
 // Staging
 elseif( ENVIRONMENT == 'staging' ):
 
-	App::set('mongodb', array(
-		'connection' => 'mongodb://user:pass@localhost:27017',
-		'database' => 'orbital'
-	));
+    App::set('mongodb', array(
+        'connection' => 'mongodb://user:pass@localhost:27017',
+        'database' => 'orbital'
+    ));
 
 // Production
 else:
 
-	App::set('mongodb', array(
-		'connection' => 'mongodb://user:pass@localhost:27017',
-		'database' => 'orbital'
-	));
+    App::set('mongodb', array(
+        'connection' => 'mongodb://user:pass@localhost:27017',
+        'database' => 'orbital'
+    ));
 
 endif;
